2d6e5d3e3a06992907d1d33830c75868bb39203a
chris0100 <alexis.rodriguez@correounivalle.edu.co>
2020-05-20 10:37:42 -0500

    New unversioned files

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/alexis/Downloads/proyectoDs2_PrimeraEntrega/proyectoDs2/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
